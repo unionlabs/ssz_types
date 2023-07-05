f1fe25fca8b89d1537611272906e1770bcd7129f
Ben Luelo <benluelo@hotmail.com>
2023-07-05 12:19:49 -0400

    feat: make modules public
    there's no reason for these to be private

--- src/lib.rs
+++ src/lib.rs
@@ -38,11 +38,11 @@
 //! ```
 
 #[macro_use]
-mod bitfield;
-mod fixed_vector;
+pub mod bitfield;
+pub mod fixed_vector;
 pub mod serde_utils;
-mod tree_hash;
-mod variable_list;
+pub mod tree_hash;
+pub mod variable_list;
 
 pub use bitfield::{BitList, BitVector, Bitfield};
 pub use fixed_vector::FixedVector;
